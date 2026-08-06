0dc419c6e55fa131b3d1b7a774cf5fbb04f81433
Vlad Dracull <81211720+jorgejs22@users.noreply.github.com>
2026-08-06 19:44:26 +0000

    Container teste

---
+++ commands/Economia/teste.js
@@ -0,0 +1,36 @@
+const {
+  SlashCommandBuilder,
+  ContainerBuilder,
+  UserSelectMenuBuilder,
+  ButtonStyle,
+  MessageFlags,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("teste")
+    .setDescription("Este comando usa Componente V2"),
+  async execute(interaction) {
+    const container = new ContainerBuilder()
+      .setAccentColor(0x0099ff)
+      .addTextDisplayComponents((text) => text.setContent(`# Algo aqui`))
+      .addSeparatorComponents((separator) => separator)
+      .addSectionComponents((section) =>
+       section.addTextDisplayComponents((text) => text.setContent(`Outra coisa aqui`),
+       (text) => text.setContent(`Continuando mais algo`))
+)
+      .addSeparatorComponents((separator) => separator)
+      .addActionRowComponents((row) =>
+        row.setComponents(
+          new UserSelectMenuBuilder()
+            .setCustomId("teste")
+            .setPlaceholder("Escolha"),
+        ),
+      );
+
+      await interaction.reply({
+        components: [container],
+        flags: MessageFlags.IsComponentV2
+      })
+  },
+};
